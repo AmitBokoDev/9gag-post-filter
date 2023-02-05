5431418c8487b3610ea42bb6341c43b9131cc5d0
AmitBokoDev <amitbokodev@gmail.com>
2023-02-05 20:57:32 +0200

    load content files

--- assets/js/content.js
+++ assets/js/content.js
@@ -1,22 +1,47 @@
-// Select the node that will be observed for mutations
-const targetNode = document.getElementById('list-view-2');
+const myTimeout = setTimeout(function(){
 
-// Options for the observer (which mutations to observe)
-const config = { attributes: true, childList: true, subtree: true };
+  // Select the node that will be observed for mutations
+  const targetNode = document.getElementById('list-view-2');
 
-// Callback function to execute when mutations are observed
-const callback = (mutationList, observer) => {
-  for (const mutation of mutationList) {
-    if (mutation.type === 'childList') {
-      console.log('A child node has been added or removed.');
-    } else if (mutation.type === 'attributes') {
-      console.log(`The ${mutation.attributeName} attribute was modified.`);
+  // Options for the observer (which mutations to observe)
+  const config = {childList: true};
+
+  var children_am = 0;
+  var settings;
+
+  chrome.storage.local.get( ['show_days',"min_days","anon","verified"], data => {
+    settings = data;
+  } ); 
+
+  // Callback function to execute when mutations are observed
+  const callback = (mutationList, observer) => {
+    for (const mutation of mutationList) {
+      if (mutation.type === 'childList') {
+        console.log('A child node has been added or removed.');
+
+        // if($("list-view-2").children.length > children_am){
+        //   children_am = $("list-view-2").children.length;
+        //   $("list-view-2").children().each(function(){
+        //     if(!this.hasClass("filtered")){
+        //       this.children("article").each(function(){
+                
+                
+        //       });
+        //     }
+        //   });
+          
+
+
+        // }
+
+      }
     }
-  }
-};
+  };
 
-// Create an observer instance linked to the callback function
-const observer = new MutationObserver(callback);
+  // Create an observer instance linked to the callback function
+  const observer = new MutationObserver(callback);
 
-// Start observing the target node for configured mutations
-observer.observe(targetNode, config);
+
+    // Start observing the target node for configured mutations
+  observer.observe(targetNode, config);
+}, 4000);
