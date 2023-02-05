510e126e54cd86215c258a648ab0e27ea800bab1
AmitBokoDev <amitbokodev@gmail.com>
2023-02-05 22:52:33 +0200

    filtering works but breaks

--- assets/js/content.js
+++ assets/js/content.js
@@ -1,3 +1,9 @@
+var settings;
+
+chrome.storage.local.get( ['show_days',"min_days","anon","verified"], data => {
+  settings = data;
+} ); 
+
 const myTimeout = setTimeout(function(){
 
   // Select the node that will be observed for mutations
@@ -6,34 +12,62 @@ const myTimeout = setTimeout(function(){
   // Options for the observer (which mutations to observe)
   const config = {childList: true};
 
-  var children_am = 0;
-  var settings;
-
-  chrome.storage.local.get( ['show_days',"min_days","anon","verified"], data => {
-    settings = data;
-  } ); 
-
-  // Callback function to execute when mutations are observed
+ 
+  // Hello there, callback hell
   const callback = (mutationList, observer) => {
     for (const mutation of mutationList) {
       if (mutation.type === 'childList') {
         console.log('A child node has been added or removed.');
+        console.log(settings);
+        $("#list-view-2").children().each(function(){
+          if(!$(this).hasClass("filtered")){
+            console.log($(this).attr("id")+" is unfiltered")
+            $(this).children("article").each(function(){
+              let art_id = $(this).attr("id");
+              let name = $("#"+art_id+" .ui-post-creator__author").text();
 
-        // if($("list-view-2").children.length > children_am){
-        //   children_am = $("list-view-2").children.length;
-        //   $("list-view-2").children().each(function(){
-        //     if(!this.hasClass("filtered")){
-        //       this.children("article").each(function(){
-                
-                
-        //       });
-        //     }
-        //   });
-          
+              if(
+                (name == "9GAGGER" && settings.anon) || //hide anons
+                ( document.querySelectorAll("#"+art_id+" .ui-post-creator__badge").length > 0 && settings.verified) // hide verified
+              ){
+                $(this).remove();
+                return;
+              }
+              if((settings.show_days || settings.min_days > 0) && name != "9GAGGER"){
+                console.log("GETting...");
+                $.get(
+                  "https://9gag.com/u/"+name,
+                  function(res){
+                    // console.log(res);
+                  res = res.substr(res.indexOf("creator"));
+                  res = res.substr(res.indexOf("creationTs")+13, 10);
+                  console.log("id: "+art_id+" name: "+name);
+                  console.log(res);
+                  res = parseInt(res);
+                  let now = Date.now()/1000;
+                  let diff = now-res;
+                  diff = diff/86400; //in days
+                  diff = parseInt(diff);
+                  console.log(settings.min_days+" ?? "+diff)
+
+                  if(settings.min_days > diff){
+                    $("#"+art_id).remove();
+                    return;
+                  }
+                  console.log("appendo");
+
+                  $("#"+art_id+" .ui-post-creator").append("| "+diff+" days");
+
+                });
 
 
-        // }
+              }             
+              
 
+            });
+            $(this).addClass("filtered");
+          }
+        });
       }
     }
   };
@@ -44,4 +78,5 @@ const myTimeout = setTimeout(function(){
 
     // Start observing the target node for configured mutations
   observer.observe(targetNode, config);
-}, 4000);
+  $("#list-view-2").append("<p></p>");
+}, 2000);
