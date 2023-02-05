0c9b1182a59e258af6ff525b938fe158acc61774
Amitb01FUB <bokobza2001@gmail.com>
2023-02-05 19:14:07 +0200

    added mutation observer example

---
+++ assets/js/content.js
@@ -0,0 +1,22 @@
+// Select the node that will be observed for mutations
+const targetNode = document.getElementById('list-view-2');
+
+// Options for the observer (which mutations to observe)
+const config = { attributes: true, childList: true, subtree: true };
+
+// Callback function to execute when mutations are observed
+const callback = (mutationList, observer) => {
+  for (const mutation of mutationList) {
+    if (mutation.type === 'childList') {
+      console.log('A child node has been added or removed.');
+    } else if (mutation.type === 'attributes') {
+      console.log(`The ${mutation.attributeName} attribute was modified.`);
+    }
+  }
+};
+
+// Create an observer instance linked to the callback function
+const observer = new MutationObserver(callback);
+
+// Start observing the target node for configured mutations
+observer.observe(targetNode, config);
