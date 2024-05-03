3227a280d924faead7c74dfc7d230dfa5c45179a
AmitBokoDev <amitbokodev@gmail.com>
2024-05-03 03:48:54 +0300

    implemented new comment filter settings saves

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,9 +11,20 @@ var $controlsCheckbox = $("#controls_checkbox");
 var $ratioCheckbox = $("#ratio_checkbox");
 var $ratioValInput = $("#ratioVal_input");
 
+var $comMinDaysInput = $("#com_min_days_input");
+var $comVerifiedCheckbox = $("#com_verified_checkbox");
+var $comProCheckbox = $("#com_pro_checkbox");
+var $comProplusCheckbox = $("#com_proplus_checkbox");
+var $comNameText = $("#com_name_text");
+var $comFlagText = $("#com_flag_text");
+
+
+
+
 // Loading settings from storage
 chrome.storage.local.get([
-  'downvotes', 'show_days', "min_days", "verified", "promoted", "spammers", "spammers_hours", "cheers", "controls", "ratio", "ratioVal"
+  'downvotes', 'show_days', "min_days", "verified", "promoted", "spammers", "spammers_hours", "cheers", "controls", "ratio", "ratioVal",
+  "comDays", "comVerified", "comPro", "comProplus", "comName", "comFlag"
 ], data => {
   $downvotesCheckbox.prop('checked', data.downvotes);
   $showDaysCheckbox.prop('checked', data.show_days);
@@ -26,6 +37,15 @@ chrome.storage.local.get([
   $controlsCheckbox.prop('checked', data.controls);
   $ratioCheckbox.prop('checked', data.ratio);  
   $ratioValInput.val(data.ratioVal);
+
+  $comMinDaysInput.val(data.comDays);
+  $comVerifiedCheckbox.prop('checked', data.comVerified);  
+  $comProCheckbox.prop('checked', data.comPro);  
+  $comProplusCheckbox.prop('checked', data.comProplus);  
+  $comNameText.val((data.comName).join(','));
+  $comFlagText.val((data.comFlag).join(','));
+
+  console.debug(data);
 });
 
 // Utility function to update Chrome storage
@@ -48,6 +68,12 @@ function setupNumberInputHandlers($input, settingName) {
   });
 }
 
+function setupTextInputHandlers($input, settingName) {
+  $input.on("change", function() {
+    updateChromeStorage(settingName, ($input.val().replaceAll(' ','')).split(','));
+  });
+}
+
 // Setting up event handlers for all checkboxes
 setupCheckboxHandlers($downvotesCheckbox, "downvotes");
 setupCheckboxHandlers($showDaysCheckbox, "show_days");
@@ -58,7 +84,26 @@ setupCheckboxHandlers($cheersCheckbox, "cheers");
 setupCheckboxHandlers($controlsCheckbox, "controls");
 setupCheckboxHandlers($ratioCheckbox, "ratio");
 
+setupCheckboxHandlers($comVerifiedCheckbox, "comVerified");
+setupCheckboxHandlers($comProCheckbox, "comPro");
+setupCheckboxHandlers($comProplusCheckbox, "comProplus");
+
 // Setting up event handlers for all inputs
 setupNumberInputHandlers($daysInput, "min_days");
 setupNumberInputHandlers($spammersInput, "spammers_hours");
 setupNumberInputHandlers($ratioValInput, "ratioVal");
+
+setupNumberInputHandlers($comMinDaysInput, "comDays");
+
+// setting up inputs with text separated by commas
+setupTextInputHandlers($comNameText, "comName")
+setupTextInputHandlers($comFlagText, "comFlag")
+
+
+// "comDays", "comVerified", "comPro", "comProplus", "comName", "comFlag"
+// var $comMinDaysInput = $("#com_min_days_input");
+// var $comVerifiedCheckbox = $("#com_verified_checkbox");
+// var $comProCheckbox = $("#com_pro_checkbox");
+// var $comProplusCheckbox = $("#com_proplus_checkbox");
+// var $comNameText = $("#com_name_text");
+// var $comFlagText = $("#com_flag_text");
